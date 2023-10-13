eb2955b72161a0d256da01583e3cb3061d20679c
kkh <k2ho33333@gmail.com>
2023-10-13 11:58:24 +0900

    1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,32 +1,57 @@
 package org.example;
-import org.example.system.controller.SystemController;
-import org.example.wisesaying.controller.WiseSayingController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     private final Scanner sc;
-    App(Scanner sc) {
+    public App(Scanner sc) {
         this.sc = sc;
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
-        SystemController systemController = new SystemController();
-        WiseSayingController wiseSayingController = new WiseSayingController(sc);
+
+        long wiseSayingNum = 1;
+
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+
         while (true) {
-            System.out.print("명령) ");
-            String command = sc.nextLine();
+            System.out.print("명언) ");
+            String command = sc.nextLine().trim();
             if (command.equals("종료")) {
-                systemController.exit();
+                System.out.println("시스템이 종료되었습니다.");
                 break;
             }
             else if (command.equals("등록")) {
-                wiseSayingController.write();
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String author = sc.nextLine().trim();
+                System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSayingNum);
+                WiseSaying wiseSaying1 = new WiseSaying(wiseSayingNum, author, content);
+                wiseSayings.add(wiseSaying1);
+                wiseSayingNum++;
             }
             else if (command.equals("목록")) {
-                wiseSayingController.list();
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                    WiseSaying wiseSaying2 = wiseSayings.get(i);
+                    System.out.printf("%d / %s / %s\n", wiseSaying2.getWiseSayingNum(), wiseSaying2.getAuthor(), wiseSaying2.getContent());
+                }
+            }
+            else if (command.startsWith("삭제")) {
+                String[] commandBits = command.split("\\?",2);
+                //삭제를 담음
+                String actionCode = commandBits[0];
+                String[] paramsBits = commandBits[1].split("&");
+                Map<String, String> params = new HashMap<>();
+                for (String paramStr : paramsBits) {
+                    String[] paramStrBits = paramStr.split("=",2);
+                    String key = paramStrBits[0];
+                    String value = paramStrBits[1];
+                    params.put(key,value);
+                }
+                System.out.println(params);
             }
         }
     }
--- src/main/java/org/example/system/controller/SystemController.java
+++
@@ -1,7 +0,0 @@
-package org.example.system.controller;
-
-public class SystemController {
-    public void exit() {
-        System.out.println("시스템이 종료됩니다.");
-    }
-}
